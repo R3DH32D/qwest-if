50c443dd805d2f15621ab156b03e324e449a5960
t4 43r <troltoh@gmail.com>
2022-09-24 10:35:21 +0800

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace FirstApplication
+{
+    public class Logic
+    {
+        public static string Compare (double circleRadius, double sideOfaSquare) 
+        {
+            string outMessage = "";
+            double circleS = Math.Pow(circleRadius, 2) * Math.PI;
+            double SquareS = Math.Pow(sideOfaSquare, 2);
+            if (Math.Abs(circleS - SquareS) >= 0.000000000001)
+            {
+                if (circleS > SquareS)
+                {
+                    outMessage = "Площадь Круга больше площади квадрата";
+                }
+
+                else if (circleS < SquareS)
+                {
+                    outMessage = "Площадь Круга меньше площади квадрата";
+                }
+            }
+            else
+            {
+                    outMessage = "Площадь Круга и площадь квадрата равны";
+            }
+            return outMessage;
+        }
+    }
+    internal class Programm 
+    {
+        static void Main(string [] args)
+        {
+            Console.WriteLine("Введите радиус круга-");
+            double circleRadius = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите сторону квадрата-");
+            double sideOfaSquare = double.Parse(Console.ReadLine());
+
+            var outMessage= Logic.Compare(circleRadius, sideOfaSquare);
+            Console.WriteLine(outMessage);
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ ConsoleApp1Tests/LogicTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FirstApplication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApplication.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void CompareTest1()
+        {
+            var circleRadius = 2;
+            var sideOfaSquare = 3;
+            var message = Logic.Compare(circleRadius, sideOfaSquare);
+            Assert.AreEqual("Площадь Круга больше площади квадрата", message);
+
+
+        }
+        [TestMethod()]
+        public void CompareTest2()
+        {
+            var circleRadius = 1;
+            var sideOfaSquare = 7;
+            var message = Logic.Compare(circleRadius, sideOfaSquare);
+            Assert.AreEqual("Площадь Круга меньше площади квадрата", message);
+        }
+        [TestMethod()]
+        public void CompareTest3()
+        {
+            var circleRadius = Math.Sqrt(Math.PI);
+            var sideOfaSquare = Math.PI;
+            var message = Logic.Compare(circleRadius, sideOfaSquare);
+            Assert.AreEqual("Площадь Круга и площадь квадрата равны", message);
+
+
+        }
+    }
+}
